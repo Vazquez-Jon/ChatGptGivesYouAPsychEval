8e37b576be09d3c9f830bfa653b68e8a22ffa5dd
JonathanVUCD <jovazquez@ucdavis.edu>
2023-02-11 19:40:58 -0800

    responses should be finished if gpt ctrl is done

--- responses.py
+++ responses.py
@@ -4,8 +4,10 @@ def parse(username, message: str, ctrl) -> str:
     lower_message = message.lower()
 
 
-    p_message = lower_message[3:]
+    p_message = lower_message[4:]
     tag = lower_message[:3]
+
+    response = ''
 
     ## Bot was called
     if tag == 'gpt':
@@ -13,10 +15,16 @@ def parse(username, message: str, ctrl) -> str:
         ## Help was called for
         ## TODO Make this useful
         if p_message == 'help':
-            return 'Make this useful'
+            response = 'Make this useful'
 
+        ## TODO Add gpt get_pyscheval
         if p_message == 'psych eval':
-            return 'pysch eval'
+            gpt_input = ctrl.db.get_data(username)
+            response  = ctrl.gpt.get_pyscheval(gpt_input)
+
+        ## Someone tried to use a command but was typed incorrectly. Is here so you dont save useless messages
+        response = 'Error: I do not know what that command was.'
 
 
-    return ''
+
+    return response
