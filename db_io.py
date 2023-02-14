4f8bdc46e17f97d290d5f8ff1f656f6bf8b1f359
JonathanVUCD <jovazquez@ucdavis.edu>
2023-02-13 16:11:20 -0800

    Added connect/disconnect func for checks

--- db_io.py
+++ db_io.py
@@ -9,6 +9,7 @@ from mysql.connector import Error
 
 
 class Database():
+    ## This block is used to see if a connection can be made
     def __init__(self) -> None:
         try:
             self.connection = mysql.connector.connect(host='golfpapaindigo-1.cbfyzw2jj1pn.us-west-2.rds.amazonaws.com',
@@ -24,6 +25,38 @@ class Database():
                 print("You're connected to database: ", self.record)
         except Error as e:
             print("Error while connecting to MySQL", e)
+        finally:
+            if self.connection.is_connected():
+                self.cursor.close()
+                self.connection.close()
+                print("MySQL connection is closed")
+
+    ## Func used to connect to database
+    def connect(self):
+        try:
+            self.connection = mysql.connector.connect(host='golfpapaindigo-1.cbfyzw2jj1pn.us-west-2.rds.amazonaws.com',
+                                                  database ='GPTGivesPsychEval',
+                                                  user='Gorroa691',
+                                                  password='FunGpTEval')
+            if self.connection.is_connected():
+                self.db_Info = self.connection.get_server_info()
+                print("Connected to MySQL Server version ", self.db_Info)
+                self.cursor = self.connection.cursor()
+                self.cursor.execute("select database();")
+                self.record = self.cursor.fetchone()
+                print("You're connected to database: ", self.record)
+        except Error as e:
+            print("Error while connecting to MySQL", e)
+    
+    ## Func used to disconnect from database
+    ## Do not want to leave left over connections
+    def disconnect(self):
+        if self.connection.is_connected():
+                self.cursor.close()
+                self.connection.close()
+                print("MySQL connection is closed")
+
+
 
     ## TODO Make db calls and append to string
     def get_data(self, user):
