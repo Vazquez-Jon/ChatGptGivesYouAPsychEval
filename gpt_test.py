bb69cf1c3ef77ca7a8644702fc7d839cd33da584
JonathanVUCD <jovazquez@ucdavis.edu>
2023-02-16 00:47:45 -0800

    Made a test of what it might actually get

--- gpt_test.py
+++ gpt_test.py
@@ -7,5 +7,6 @@ import gpt_control
 
 gpt = gpt_control.GPT_Control()
 
-print(gpt.get_psych_eval('What is dark matter?')+'/n')
-print(gpt.get_psych_eval('What are farts made of?')+'/n')
+print("Give me a psych eval based on someone that talks in the following way.\n This gonna happen to @lasercats one day\n This picture made me go braindead\n Yeah Joel, why won't you tell them that?\n@Colonel Custard just realized that since new raid is 48hrs now, if we get it done early we can try to take you through it on sat if you want\n @Colonel Custard just realized that since new raid is 48hrs now, if we get it done early we can try to take you through it on sat if you want")
+print(gpt.get_psycheval("Give me a psych eval based on someone that talks in the following way.\n This gonna happen to @lasercats one day\n This picture made me go braindead\n Yeah Joel, why won't you tell them that?\n @Colonel Custard just realized that since new raid is 48hrs now, if we get it done early we can try to take you through it on sat if you want\n @Colonel Custard just realized that since new raid is 48hrs now, if we get it done early we can try to take you through it on sat if you want"))
+print(gpt.get_psycheval('What are farts made of?'))
