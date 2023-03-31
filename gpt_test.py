8d3090cb7c47953e3b2eff31ea9b6eae27ae275c
JonathanVUCD <jovazquez@ucdavis.edu>
2023-03-31 12:14:19 -0700

    Updated test to use chat get completion

--- gpt_test.py
+++ gpt_test.py
@@ -4,9 +4,14 @@
 ## Desc: Simple function used to see if the gpt_control.py class works
 
 import gpt_control
+import chat_gpt_control
 
 gpt = gpt_control.GPT_Control()
+chat_gpt = chat_gpt_control.Chat_GPT_Control()
 
-print("Give me a psych eval based on someone that talks in the following way.\n This gonna happen to @lasercats one day\n This picture made me go braindead\n Yeah Joel, why won't you tell them that?\n@Colonel Custard just realized that since new raid is 48hrs now, if we get it done early we can try to take you through it on sat if you want\n @Colonel Custard just realized that since new raid is 48hrs now, if we get it done early we can try to take you through it on sat if you want")
-print(gpt.get_psycheval("Give me a psych eval based on someone that talks in the following way.\n This gonna happen to @lasercats one day\n This picture made me go braindead\n Yeah Joel, why won't you tell them that?\n @Colonel Custard just realized that since new raid is 48hrs now, if we get it done early we can try to take you through it on sat if you want\n @Colonel Custard just realized that since new raid is 48hrs now, if we get it done early we can try to take you through it on sat if you want"))
-print(gpt.get_psycheval('What are farts made of?'))
+input = "Give me a psych eval based on someone that talks in the following way.\n This gonna happen to @lasercats one day\n This picture made me go braindead\n Yeah Joel, why won't you tell them that?\n@Colonel Custard just realized that since new raid is 48hrs now, if we get it done early we can try to take you through it on sat if you want\n @Colonel Custard just realized that since new raid is 48hrs now, if we get it done early we can try to take you through it on sat if you want"
+##print('GPT responses')
+##print(gpt.get_psycheval(input))
+##print(gpt.get_psycheval('What are farts made of?'))
+print('\nChat GPT Completions')
+print(chat_gpt.get_psycheval(input))
